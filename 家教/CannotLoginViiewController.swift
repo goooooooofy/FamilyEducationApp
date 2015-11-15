da337fba0848aa63eae6cac62f3a1d11d6fa41b3
goofyy <1163191060@qq.com>
2015-11-15 22:56:28 +0800

    完善注册界面

---
+++ 家教/CannotLoginViiewController.swift
@@ -0,0 +1,13 @@
+//
+//  CannotLoginViiewController.swift
+//  家教
+//
+//  Created by goofygao on 15/11/15.
+//  Copyright © 2015年 goofyy. All rights reserved.
+//
+
+import Cocoa
+
+class CannotLoginViiewController: UIViewController {
+
+}
