a3b4134fe32230d09f59e60426b7c154f1a95af8
goofyy <1163191060@qq.com>
2015-11-16 21:26:56 +0800

    完善注册collection

--- 家教/SignUpViewController.swift
+++ 家教/SignUpViewController.swift
@@ -16,13 +16,16 @@ class SignUpViewController: UIViewController {
     
     @IBOutlet weak var proViewLine: UIView!
     
+    var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         initView()
+        
     }
     
     func initView() {
-        
+       
         self.navigationController?.navigationBarHidden = false
         self.view.backgroundColor = UIColor(red: 65.0 / 255.0, green: 62.0 / 255.0, blue: 79.0 / 255.0, alpha: 1)
         let navBar = self.navigationController?.navigationBar
@@ -33,6 +36,18 @@ class SignUpViewController: UIViewController {
         phoneSignButton.addTarget(self, action: "phoneSignAction:", forControlEvents: UIControlEvents.TouchDown)
         emailSignButton.addTarget(self, action: "emailSianAction:", forControlEvents: UIControlEvents.TouchDown)
         
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSizeMake(DeviceData.width, DeviceData.height - CGRectGetMaxY(proViewLine.frame))
+        layout.minimumLineSpacing = 0
+        layout.scrollDirection = UICollectionViewScrollDirection.Horizontal
+        collectionView = UICollectionView(frame: CGRectMake(0, CGRectGetMaxY(proViewLine.frame), DeviceData.width, DeviceData.height - CGRectGetMaxY(proViewLine.frame)), collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "collection")
+        self.collectionView?.pagingEnabled = true
+        self.collectionView?.bounces = false
+        self.collectionView?.showsHorizontalScrollIndicator = false
+        self.view.addSubview(collectionView)
     }
     
     /**
@@ -40,15 +55,58 @@ class SignUpViewController: UIViewController {
     */
     func phoneSignAction(sender:UIButton) {
         sender.tintColor = UIColor.blueColor()
-        UIView.animateWithDuration(0.7, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+        UIView.animateWithDuration(0.5, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
             self.proViewLine.frame = CGRectMake(0, self.proViewLine.frame.origin.y, DeviceData.width/2, 5)
             }, completion: nil)
+        
+        self.collectionView.contentOffset.x += DeviceData.width
+        
     }
     
     func emailSianAction(sender:UIButton) {
         sender.tintColor = UIColor.blueColor()
-        UIView.animateWithDuration(0.7, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+        UIView.animateWithDuration(0.5, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
             self.proViewLine.frame = CGRectMake(DeviceData.width/2, self.proViewLine.frame.origin.y, DeviceData.width/2, 5)
             }, completion: nil)
+        self.collectionView.contentOffset.x -= DeviceData.width
+         self.collectionView.reloadData()
     }
+    
 }
+
+// MARK: - UICollectionView代理
+
+extension SignUpViewController: UICollectionViewDelegate,UICollectionViewDataSource {
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let collection = collectionView.dequeueReusableCellWithReuseIdentifier("collection", forIndexPath: indexPath) 
+        if indexPath.row == 0 {
+            collection.backgroundColor = UIColor.redColor()
+        } else {
+            collection.backgroundColor = UIColor.greenColor()
+        }
+        return collection
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        let page = scrollView.contentOffset.x / DeviceData.width
+        if page == 0 {
+            UIView.animateWithDuration(0.5, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+                self.proViewLine.frame = CGRectMake(0, self.proViewLine.frame.origin.y, DeviceData.width/2, 5)
+                }, completion: nil)
+        } else {
+            UIView.animateWithDuration(0.5, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+                self.proViewLine.frame = CGRectMake(DeviceData.width/2, self.proViewLine.frame.origin.y, DeviceData.width/2, 5)
+                }, completion: nil)
+        }
+        }
+    }
+    
+    
